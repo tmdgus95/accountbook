c1c5d3c1c7a032eee093b2c5ef9b6645997c7ec9
tmdgus95 <8571053@naver.com>
2023-02-10 16:50:35 +0900

    기본 컴포넌트 생성

---
+++ src/components/SelectOption.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SelectOption = () => {
+    return <div>SelectOption</div>;
+};
+
+export default SelectOption;
---
+++ src/components/expendModal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const expendModal = () => {
+    return <div>expendModal</div>;
+};
+
+export default expendModal;
