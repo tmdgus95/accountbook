168333b3dde3ef7dc816104890edc6fd6389ce0f
tmdgus95 <8571053@naver.com>
2023-02-10 15:06:45 +0900

    폴더구조 잡기

---
+++ src/api/axios.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const axios = () => {
+    return <div>axios</div>;
+};
+
+export default axios;
---
+++ src/hooks/useMoney.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function useMoney() {
+    return <div></div>;
+}
+
+export default useMoney;
