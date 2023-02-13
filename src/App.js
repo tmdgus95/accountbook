c27ad5a74d71fe1884ec253f1dfacca3480c39be
tmdgus95 <8571053@naver.com>
2023-02-13 16:20:45 +0900

    라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Outlet } from "react-router-dom";
+import BottomNav from "./components/BottomNav";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
-    return <></>;
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <BottomNav />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Login from "./pages/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Chart from "./pages/Chart";
+import Statistics from "./pages/Statistics";
+import Calendar from "./pages/Calendar";
+import Mypage from "./pages/Mypage";
+import SignUp from "./pages/SignUp";
+import ErrorPage from "./pages/ErrorPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            { index: true, element: <Home /> },
+            { path: "calendar", element: <Calendar /> },
+            { path: "chart", element: <Chart /> },
+            { path: "statistics", element: <Statistics /> },
+            { path: "mypage", element: <Mypage /> },
+            { path: "login", element: <Login /> },
+            { path: "signup", element: <SignUp /> },
+        ],
+    },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+    // <React.StrictMode>
+    <RouterProvider router={router} />
+    // </React.StrictMode>
 );
