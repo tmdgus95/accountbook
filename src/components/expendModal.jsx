e0cf7168b14c8ad74c79d0291e31e80aab3f8d3a
Park Sieun <pmj8856@naver.com>
2023-02-15 17:47:03 +0900

    ExpendModal 기본 틀 구성중입니다.

--- src/components/expendModal.jsx
+++ src/components/expendModal.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-const expendModal = () => {
-    return <div>expendModal</div>;
+const schema = yup
+    .object({
+        price: yup
+            .number("숫자를 입력하세요")
+            .positive("양수를 입력하세요")
+            .integer("정수를 입력하세요")
+            .required("금액을 입력하세요"),
+        memo: yup.string("문자를 입력하세요").required("메모를 입력하세요"),
+    })
+    .required();
+
+const ExpendModal = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    const onSubmit = (data) => console.log(data);
+
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>
+                    금액 <input {...register("price")} />
+                </label>
+                <p>{errors.price?.message}</p>
+
+                <label>
+                    메모 <input {...register("memo")} />
+                </label>
+                <p>{errors.memo?.message}</p>
+
+                <button>저장</button>
+            </form>
+        </>
+    );
 };
 
-export default expendModal;
+export default ExpendModal;
