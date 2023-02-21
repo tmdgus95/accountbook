cd2ed1f538a1251fbfede4773f4aec913b3db101
Park Sieun <pmj8856@naver.com>
2023-02-21 19:01:08 +0900

    ExpendModal 기능 수정, Calendar 내용 출력 기능 수정중입니다.

--- src/pages/MoneyCalendar.jsx
+++ src/pages/MoneyCalendar.jsx
@@ -5,7 +5,8 @@ import "react-calendar/dist/Calendar.css"; // css import
 import moment from "moment/moment";
 // 한글로 출력하게 해줌.
 import "moment/locale/ko";
-import styled from "styled-components";
+import { useEffect } from "react";
+import axios from "axios";
 
 const MoneyCalendar = () => {
     // 로컬 정보 호출
@@ -18,14 +19,49 @@ const MoneyCalendar = () => {
         }
     };
 
+    useEffect(() => {
+        console.log("날짜호출");
+        const body = { saiSeq: "1", year: "2023", month: "02" };
+        axios
+            .get("http://192.168.0.208:9090/api/accountbook/list/month", body)
+            .then((res) => console.log(res));
+    }, []);
+
     const [todoData, setTodoData] = useState(getLocalPost());
     // 선택된 날짜
     const [date, setDate] = useState(new Date());
     // 이미지 출력
     const publicFolder = process.env.PUBLIC_URL;
 
+    // 캘린더 내용 출력
+    const showCaledar = ({ date, view }) => {
+        console.log("넘어오니?", date);
+    };
+
+    const showTile = ({ date, view }) => {
+        let html = [];
+        let obj = todoData.find((item, index) => {
+            if (item.timestamp === moment(date).format("YYYY-MM-DD")) {
+                return item;
+            }
+        });
+
+        if (obj !== undefined) {
+            html.push(
+                <div key={obj.timestamp}>
+                    <span>{obj.title}</span>
+                    <span>{obj.content}</span>
+                </div>
+            );
+
+            return <div>{html}</div>;
+        }
+
+        return null;
+    };
+
     return (
-        <Wrap>
+        <>
             <Calendar
                 formatDay={(locale, date) => moment(date).format("D")}
                 // 일요일부터 출력
@@ -33,34 +69,7 @@ const MoneyCalendar = () => {
                 // 날짜 선택시 날짜 변경
                 onChange={setDate}
                 // 달력에 출력될 html 작성
-                tileContent={({ date, view }) => {
-                    let html = [];
-                    // console.log(date);
-                    // date      : Tue Feb 28 2023 00:00:00 GMT+0900 (한국 표준시)
-                    // item.date : "2023-02-07"
-                    // 각각의 날짜 영역에 출력하고 싶은 내용을 작성한다.
-                    // 날짜를 비교해서 같으면 출력을 하겠다.
-                    if (
-                        todoData.find((item, index) => {
-                            // 현재 date 는 포맷이 다릅니다.
-                            return (
-                                item.timestamp ===
-                                moment(date).format("YYYY-MM-DD")
-                            );
-                        })
-                    ) {
-                        // 조건에 맞으므로 html 을 생성해준다.
-                        html.push(
-                            <img
-                                key={`todoData${moment(date)}`}
-                                src={`${publicFolder}/images/starbucks.png`}
-                                alt="아이콘"
-                                style={{ width: 20, height: 20 }}
-                            />
-                        );
-                    }
-                    return <div>{html}</div>;
-                }}
+                tileContent={showTile}
             />
             {/* 상세 정보 내역 출력 */}
             <div className="calender-detail">
@@ -82,14 +91,8 @@ const MoneyCalendar = () => {
                     </div>
                 )}
             </div>
-        </Wrap>
+        </>
     );
 };
-
-const Wrap = styled.div`
-    .react-calendar__month-view__days__day--weekend {
-        color: blue;
-    }
-`;
 
 export default MoneyCalendar;
